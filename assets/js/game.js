9856b87495959836e556cb0b2413a78b258f9e2a
Kodama <kodama@suichu.net>
2023-06-08 15:57:37 +0900

    かっぱの画像移動

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,143 +1,93 @@
-var Game = {
-  display: null,
+//canvas設定
+const canvas = document.getElementById('canvas')
+canvas.width = 640;
+canvas.height = 640;
 
-  init: function() {
-      this.display = new ROT.Display();
-      document.body.appendChild(this.display.getContainer());
+//コンテキストを取得
+const context = canvas.getContext('2d')
+
+//キャラ画像
+const kappa = new Object();
+kappa.img = new Image();
+kappa.img.src = '/assets/img/kappa.png'
+kappa.x = 0;
+kappa.y = 0;
+kappa.move = 0;
+
+//キーボードのオブジェクト
+const key = new Object();
+key.up = false;
+key.down = false;
+key.right = false;
+key.left = false;
+key.push = '';
+
+//メインループ
+function main() {
+  //塗（ぬ）りつぶす色を指定（してい）
+	context.fillStyle = "rgb( 0, 0, 0 )";
+	//塗（ぬ）りつぶす
+	context.fillRect(0, 0, 640, 640);
+
+  //画像の表示
+  context.drawImage( kappa.img, kappa.x, kappa.y );  
+
+  //キーボード押された時、話した時のイベント
+  addEventListener('keydown', keydownfunc, false);
+  addEventListener('keyup', keyupfunc, false);
+
+  //方向キーが押されている場合、かっぱ移動
+  if (kappa.move === 0) {
+    if (key.left === true) {
+      kappa.move = 32;
+      key.push = 'left';
+    }
+    if (key.up === true) {
+      kappa.move = 32;
+      key.push = 'up';
+    }
+    if (key.right === true) {
+      kappa.move = 32;
+      key.push = 'right';
+    }
+    if (key.down === true) {
+      kappa.move = 32;
+      key.push = 'down';
+    }
   }
-}
-
-Game.map = {};
-Game._generateMap = function() {
-  var digger = new ROT.Map.Digger();
-  var freeCells = [];
-
-  var digCallback = function(x, y, value) {
-      if (value) { return; } /* do not store walls */
-
-      var key = x+","+y;
-      freeCells.push(key);
-      this.map[key] = ".";
+  
+  //kappa.moveが０より大きい場合場合は、4pxずるつ移動
+  if (kappa.move > 0) {
+    kappa.move -= 4;
+    if (key.push === 'left') kappa.x -= 4;
+    if (key.push === 'up') kappa.y -= 4;
+    if (key.push === 'right') kappa.x += 4;
+    if (key.push === 'down') kappa.y += 4;
   }
-  digger.create(digCallback.bind(this));
 
-  this._generateBoxes(freeCells);
+  requestAnimationFrame( main );
 
-  this._drawWholeMap();
-};
-
-Game.ananas = null;
-
-Game._generateBoxes = function(freeCells) {
-  for (var i=0;i<10;i++) {
-      var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
-      var key = freeCells.splice(index, 1)[0];
-      this.map[key] = "*";
-      if (!i) { this.ananas = key; } /* first box contains an ananas */
-  }
-};
-
-Game._drawWholeMap = function() {
-  for (var key in this.map) {
-      var parts = key.split(",");
-      var x = parseInt(parts[0]);
-      var y = parseInt(parts[1]);
-      this.display.draw(x, y, this.map[key]);
-  }
 }
 
-//プレイヤーキャラクター
-var Player = function(x, y) {
-  this._x = x;
-  this._y = y;
-  this._draw();
+//画像の表示
+addEventListener('load', main(), false);
+
+//キーボードが押されたときに呼び出される関数（かんすう）
+function keydownfunc( event ) {
+	const key_code = event.keyCode;
+	if( key_code === 37 ) key.left = true;
+	if( key_code === 38 ) key.up = true;
+	if( key_code === 39 ) key.right = true;
+	if( key_code === 40 ) key.down = true;
+	event.preventDefault();
 }
 
-Player.prototype._draw = function() {
-  Game.display.draw(this._x, this._y, "@", "#ff0");
+//キーボードが放（はな）されたときに呼び出される関数
+function keyupfunc( event ) {
+	const key_code = event.keyCode;
+	if( key_code === 37 ) key.left = false;
+	if( key_code === 38 ) key.up = false;
+	if( key_code === 39 ) key.right = false;
+	if( key_code === 40 ) key.down = false;
 }
 
-Game.player = null;
-
-Game._generateMap = function() {
-  /* ...previous stuff... */
-  this._createPlayer(freeCells);
-};
-
-Game._createPlayer = function(freeCells) {
-  var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
-  var key = freeCells.splice(index, 1)[0];
-  var parts = key.split(",");
-  var x = parseInt(parts[0]);
-  var y = parseInt(parts[1]);
-  this.player = new Player(x, y);
-};
-
-
-//ゲームターンエンジンの準備
-Game.engine = null;
-
-Game.init = function() {
-    var scheduler = new ROT.Scheduler.Simple();
-    scheduler.add(this.player, true);
-    this.engine = new ROT.Engine(scheduler);
-    this.engine.start();
-}
-
-//アクターとエンジン間のインタラクション
-Player.prototype.act = function() {
-  Game.engine.lock();
- /* ユーザー入力を待ちます。ユーザーがキーを押したときに処理を実行します */
-  window.addEventListener("keydown", this);
-}
-
-   /* ユーザー入力を処理します */
-Player.prototype.handleEvent = function(e) {
-
-   //キーボードを操作してプレーヤーを移動する
-  var keyMap = {};
-  keyMap[38] = 0;
-  keyMap[33] = 1;
-  keyMap[39] = 2;
-  keyMap[34] = 3;
-  keyMap[40] = 4;
-  keyMap[35] = 5;
-  keyMap[37] = 6;
-  keyMap[36] = 7;
-
-  var code = e.keyCode;
-  if (code == 13 || code == 32) {
-    this._checkBox();
-    return;
-  } 
-
-  if (!(code in keyMap)) { return; }
-
-  var diff = ROT.DIRS[8][keyMap[code]];
-  var newX = this._x + diff[0];
-  var newY = this._y + diff[1];
-
-  var newKey = newX + "," + newY;
-  if (!(newKey in Game.map)) { return; }  /* この方向には移動できません */ 
-
-  Game.display.draw(this._x, this._y, Game.map[this._x+","+this._y]);
-  this._x = newX;
-  this._y = newY;
-  this._draw();
-  window.removeEventListener("keydown", this);
-  Game.engine.unlock();
-}
-
-
-Player.prototype._checkBox = function() {
-  var key = this._x + "," + this._y;
-  if (Game.map[key] != "*") {
-      alert("ここにはボックスがありません!");
-  } else if (key == Game.ananas) {
-      alert(" 「万歳！あなたはアナナスを見つけて、このゲームに勝ちました。」");
-      Game.engine.lock();
-      window.removeEventListener("keydown", this);
-  } else {
-      alert("このボックスは空です :-(");
-  }
-}
